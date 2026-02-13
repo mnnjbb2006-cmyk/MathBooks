c0bc818ae3bf09cb71d1c8d3f64ce31700015a68
Mani Jabbari <mnnjbb.2006@gmail.com>
2026-02-13 15:11:22 +0330

    number of books fixed and publisher field has been added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests, csv
 def get_books(limit=50):
     url = "https://openlibrary.org/search.json"
     headers = {"User-agent":"Mathbooks (mnnjbb.2006@gmail.com)"}
-    params = {"q":"", "limit":limit, "subject":"math"}
+    params = {"q":"", "limit":limit, "subject":"math", "fields":["title", "author_name", "first_publish_year", "publisher"]}
     try:
         res = requests.get(url, headers=headers, params=params)
         return res.json().get("docs",[])
@@ -11,10 +11,10 @@ def get_books(limit=50):
         print(f"Error: {e}")
         exit()
 
-books = get_books()
+books = get_books(50)
 
 with open("books.csv", "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["Title", "Author", "Year"])
+    writer.writerow(["Title", "Author", "Year", "Publisher"])
     for book in books:
-        writer.writerow([book.get("title", ""), book.get("author_name", [""])[0], book.get("first_publish_year", "")])
+        writer.writerow([book.get("title", ""), book.get("author_name", [""])[0], book.get("first_publish_year", ""), book.get("publisher", [""])[0]])
